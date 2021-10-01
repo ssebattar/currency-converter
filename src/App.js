cebab7b02a36cfa7b8e10dba720a186a27e1dee7
Richard S <rssebatta@gmail.com>
2021-10-01 15:42:24 -0400

    Updated project info

--- src/App.js
+++ src/App.js
@@ -1,15 +1,157 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import Dropdown from "react-dropdown";
+import { HiSwitchHorizontal } from "react-icons/hi";
+
+import "react-dropdown/style.css";
 import "./App.css";
+import Userguide from "./components/UserGuide/Userguide";
+import img_header from "./img-header.jpg";
 
 function App() {
+  // Initializing all the state variables
+  const [info, setInfo] = useState([]);
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("jpy");
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
+  const [names, setNames] = useState([]);
+
+  // Calling the api whenever the dependency changes
+  useEffect(() => {
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`
+    ).then((res) => {
+      setInfo(res.data[from]);
+    });
+  }, [from]);
+
+  // Set name
+  useEffect(() => {
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json`
+    ).then((res) => {
+      setNames(res.data);
+    });
+  });
+
+  // Calling the convert function whenever
+  // a user switches the currency
+  useEffect(() => {
+    setOptions(Object.keys(info));
+    convert();
+  }, [info]);
+
+  // Function to convert the currency
+  var rate = info[to];
+  function convert() {
+    setOutput(input * rate);
+  }
+
+  // Function to switch between two currency
+  function flip() {
+    var temp = from;
+    setFrom(to);
+    setTo(temp);
+  }
+
+  /* var curr = "./currencies.json";
+  var c = JSON.parse(curr);
+  var r = [];
+  for (var i in c) r.push(c[i]); */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is project CMSC 495 skeleton - Currency Converter App for Group 6
-        </p>
-      </header>
+    <div className="outer">
+      <div className="heading">
+        <h1>Project 495 Group 6 - Currency Converter</h1>
+      </div>
+
+      <div className="inner">
+        <div className="user-guide">
+          <Userguide />
+        </div>
+
+        <div className="App">
+          <div className="img-header">
+            <img src={img_header} width="900" height="150" />
+          </div>
+          <div className="container">
+            <div className="left">
+              <h3>Amount</h3>
+              <input
+                type="text"
+                placeholder="Enter the amount"
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </div>
+            <div className="middle">
+              <h3>From</h3>
+              <Dropdown
+                options={options}
+                onChange={(e) => {
+                  setFrom(e.value);
+                }}
+                value={from.toUpperCase()}
+                placeholder="From"
+              />
+            </div>
+            <div className="switch">
+              <HiSwitchHorizontal
+                size="30px"
+                onClick={() => {
+                  flip();
+                }}
+              />
+            </div>
+            <div className="right">
+              <h3>To</h3>
+              <Dropdown
+                options={options}
+                onChange={(e) => {
+                  setTo(e.value);
+                }}
+                value={to.toUpperCase()}
+                placeholder="To"
+              />
+            </div>
+          </div>
+          <div className="result">
+            <button
+              onClick={() => {
+                convert();
+              }}
+            >
+              Convert
+            </button>
+            <div className="res">
+              <div className="res-ins">
+                <h3>Converted Amount:</h3>
+                <p>
+                  {input +
+                    " " +
+                    from.toUpperCase() +
+                    " = " +
+                    output.toFixed(2) +
+                    " " +
+                    to.toUpperCase()}
+                </p>
+              </div>
+              <div className="res-ins">
+                <h3>Rate:</h3>
+                <p>
+                  {"1 " +
+                    from.toUpperCase() +
+                    " = " +
+                    rate +
+                    " " +
+                    to.toUpperCase()}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/UserGuide/Userguide.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Userguide.css";
+
+const Userguide = () => {
+  return (
+    <div className="text-wrapper">
+      <h2>User Guide</h2>
+      Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
+      egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
+      hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
+      habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+      sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
+      <br />
+      <br />
+      <b>Sub Header</b>
+      <br />
+      Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras
+      porttitor condimentum orci suscipit. Leo maecenas in tristique, himenaeos
+      elementum placerat. Taciti rutrum nostra, eget cursus velit ultricies.
+      Quam molestie tellus himenaeos cubilia congue vivamus ultricies. Interdum
+      praesent ut penatibus fames eros ad consectetur sed.
+      <br />
+      <br />
+      <b>Sub Header</b>
+      <br />
+      Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras
+      porttitor condimentum orci suscipit. Leo maecenas in tristique, himenaeos
+      elementum placerat. Taciti rutrum nostra, eget cursus velit ultricies.
+      Quam molestie tellus himenaeos cubilia congue vivamus ultricies. Interdum
+      praesent ut penatibus fames eros ad consectetur sed.
+    </div>
+  );
+};
+
+export default Userguide;
