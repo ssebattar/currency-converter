9135ef75ecae4f24b6fbb537971c80f0a746aba1
Richard S <rssebatta@gmail.com>
2021-10-05 15:21:15 -0400

    updated userguide format

--- src/components/UserGuide/Userguide.js
+++ src/components/UserGuide/Userguide.js
@@ -5,14 +5,33 @@ const Userguide = () => {
   return (
     <div className="text-wrapper">
       <h2>User Guide</h2>
+      <b>How To Use</b>
+      <br />
       Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
       egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
       hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
       habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
       sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
       <br />
+      <a a href="">
+        Read More...
+      </a>
       <br />
-      <b>Sub Header</b>
+      <br />
+      <b>System Requirements</b>
+      <br />
+      Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras
+      porttitor condimentum orci suscipit. Leo maecenas in tristique, himenaeos
+      elementum placerat. Taciti rutrum nostra, eget cursus velit ultricies.
+      Quam molestie tellus himenaeos cubilia congue vivamus ultricies. Interdum
+      praesent ut penatibus fames eros ad consectetur sed.
+      <br />
+      <a a href="">
+        Read More...
+      </a>
+      <br />
+      <br />
+      <b>Installation</b>
       <br />
       Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras
       porttitor condimentum orci suscipit. Leo maecenas in tristique, himenaeos
@@ -20,14 +39,22 @@ const Userguide = () => {
       Quam molestie tellus himenaeos cubilia congue vivamus ultricies. Interdum
       praesent ut penatibus fames eros ad consectetur sed.
       <br />
+      <a a href="">
+        Read More...
+      </a>
       <br />
-      <b>Sub Header</b>
+      <br />
+      <b>Currencies</b>
       <br />
       Non etiam tempor id arcu magna ante eget. Nec per posuere cubilia cras
       porttitor condimentum orci suscipit. Leo maecenas in tristique, himenaeos
       elementum placerat. Taciti rutrum nostra, eget cursus velit ultricies.
       Quam molestie tellus himenaeos cubilia congue vivamus ultricies. Interdum
       praesent ut penatibus fames eros ad consectetur sed.
+      <br />
+      <a a href="">
+        Read More...
+      </a>
     </div>
   );
 };
