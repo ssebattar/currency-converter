4fb8cb9a0cda2e5f9f43a21bbdc3d38dcee3ba13
Richard <63207045+ssebattar@users.noreply.github.com>
2021-10-09 16:22:26 -0400

    Update FullUserguide.js

--- src/components/UserGuide/FullUserguide.js
+++ src/components/UserGuide/FullUserguide.js
@@ -19,9 +19,22 @@ const FullUserguide = () => {
         <br />
         Use the Currency Converter to calculate exchange rates for any day. Type
         currency names, 3-letter ISO currency symbols, or country names to
-        select your currency. Type values in either side of the converter,
-        depending on if you have money to exchange or if you want money to
-        purchase items costing a certain amount.
+        select your currency. Type values in enter amount box of the converter.
+        <br />
+        1) Enter the amount
+        <br />
+        2) Select the currency you are converting from
+        <br />
+        3) Select the currency you are converting to
+        <br />
+        4) Press the convert button
+        <br />
+        5) Observe the results. The exchange rate and the converted amount.
+        <br />
+        <br />
+        <div className="img-usrg">
+          <img src={img_userguide} width="500" height="300" />
+        </div>
         <br />
         <br />
         2. Why can't I find the currency I'm looking for?
